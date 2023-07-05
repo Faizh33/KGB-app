c9da13fcb7bf516cf9ffe4ed9367bf177e12ae5d
Erika REDON <erika_redon@hotmail.com>
2023-07-05 20:46:09 +0200

    adding comments

--- app/controllers/insertAgentController.php
+++ app/controllers/insertAgentController.php
@@ -6,8 +6,9 @@ include_once "../classes/Agent.php";
 
 use app\classes\Agent;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //Vérifier si les informations sont passées dans le POST
     if(isset($_POST["agentLastName"]) && isset($_POST["agentFirstName"]) && isset($_POST["agentBirthDate"]) && isset($_POST["agentNationality"]) && isset($_POST["agentIdCode"])) {
         $lastName = valid_datas($_POST["agentLastName"]);
         $firstName = valid_datas($_POST["agentFirstName"]);
@@ -15,14 +16,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $nationality = valid_datas($_POST["agentNationality"]);
         $idCode = valid_datas($_POST["agentIdCode"]);
 
+        //Création d'un nouvel objet Agent et insertion des données
         $agent = new Agent($pdo);
         $agent->addAgentProperties($pdo, $lastName, $firstName, $birthDate, $nationality, $idCode);
         
+        //Si l'ajout en base de données à réussi : redirection vers la page de création
         if(isset($agent)) {
             echo "<div style='font-weight:bold;color:rgb(3, 114, 103)'>Nouvel agent ajouté en base de données</div>";
             header('refresh:5;Location: ../views/dashboardCreate.php');
         }
     }
 }
-
-//addAgentProperties(\PDO $pdo, string $lastName, string $firstName, string $birthDate, string $nationality, string $identificationCode)
