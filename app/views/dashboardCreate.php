1a06223b801af912ce4c888c7db1de4dc57217a5
Erika REDON <erika_redon@hotmail.com>
2023-07-06 10:52:12 +0200

    create back button

--- app/views/dashboardCreate.php
+++ app/views/dashboardCreate.php
@@ -16,6 +16,7 @@
 
     <?php if (isset($_SESSION['admin']) && $_SESSION['admin'] === true) :?>
     <div class="dashboardContainer">
+        <!-- Contenu du tableau de bord -->
         <table>
             <tr>
                 <td>
@@ -90,6 +91,15 @@
     </div>    
     <div class="formContainer" id="typeForm" style="display:none;">
         <?php include_once "../snippets/create-form/newTypeForm.php"; ?>
+    </div>
+    <!-- Bouton de retour arrière -->
+    <div class="backButtonContainer" style="display:none;">
+        <button class="backButton">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.5vw" height="1.5vw" fill="currentColor" color="white" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+            </svg>
+            <span class="backText">Retour</span>
+        </button>
     </div>
     <?php endif; ?>
 
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,23 +1,45 @@
-    document.addEventListener("DOMContentLoaded", function() {
-        var buttons = document.getElementsByClassName("dashboardButton");
-        var dashboardContainer = document.querySelector(".dashboardContainer");
+document.addEventListener("DOMContentLoaded", function() {
+    //Sélection des différents éléments
+    var buttons = document.getElementsByClassName("dashboardButton");
+    var dashboardContainer = document.querySelector(".dashboardContainer");
+    var backButtonContainer = document.querySelector(".backButtonContainer");
 
-        for (var i = 0; i < buttons.length; i++) {
-            var button = buttons[i];
-            button.addEventListener("click", function(event) {
-                var formId = event.target.getAttribute("data-form");
-                var forms = document.getElementsByClassName("formContainer");
+    // Boucle sur les boutons du tableau de bord, ajoute un gestionnaire de clic et récupère l'ID du formulaire
+    for (var i = 0; i < buttons.length; i++) {
+        var button = buttons[i];
+        button.addEventListener("click", function(event) {
+            var formId = event.target.getAttribute("data-form");
+            var forms = document.getElementsByClassName("formContainer");
 
-                for (var j = 0; j < forms.length; j++) {
-                    forms[j].style.display = "none";
-                }
+            // Masque tous les formulaires en définissant leur style d'affichage sur "none"
+            for (var j = 0; j < forms.length; j++) {
+                forms[j].style.display = "none";
+            }
 
-                var form = document.getElementById(formId);
-                if (form) {
-                    form.style.display = "block";
-                }
+            var form = document.getElementById(formId);
 
-                dashboardContainer.style.display = "none";
-            });
+            // Affiche le formulaire
+            if (form) {
+                form.style.display = "block";
+            }
+
+            // Masque le container du tableau de bord et affiche le container du bouton de retour
+            dashboardContainer.style.display = "none";
+            backButtonContainer.style.display = "block";
+        });
+    }
+
+    var backButton = document.querySelector(".backButton");
+
+    //Sélectionne les formulaires et les masque
+    backButton.addEventListener("click", function() {
+        var forms = document.getElementsByClassName("formContainer");
+        for (var j = 0; j < forms.length; j++) {
+            forms[j].style.display = "none";
         }
+
+        // Affiche le container du tableau de bord et masque le container du bouton de retour
+        dashboardContainer.style.display = "block";
+        backButtonContainer.style.display = "none";
     });
+});
