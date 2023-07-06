d72ed492cb1533bca8b4461397d5c2a0faec0fae
Erika REDON <erika_redon@hotmail.com>
2023-07-06 19:08:17 +0200

    create controller to insert mission datas in database

---
+++ app/controllers/insertMissionController.php
@@ -0,0 +1,61 @@
+<?php
+
+use app\classes\Mission;
+use app\classes\Speciality;
+use app\classes\MissionStatus;
+use app\classes\MissionType;
+
+include_once "../../config/database.php";
+include_once "../helpers/dataHelpers.php";
+include_once "../classes/Mission.php";
+include_once "../classes/Speciality.php";
+include_once "../classes/MissionStatus.php";
+include_once "../classes/MissionType.php";
+
+
+// Vérifie si la méthode de requête est POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Récupère les données soumises via POST
+    if(isset($_POST["title"]) && isset($_POST["description"]) && isset($_POST["codeName"]) && isset($_POST["country"]) && isset($_POST["startDate"]) && isset($_POST["endDate"]) && isset($_POST["missionType"]) && isset($_POST["missionSpeciality"]) && isset($_POST["missionStatus"]) && isset($_POST["agents"]) && isset($_POST["contacts"]) && isset($_POST["targets"]) && isset($_POST["safeHouses"])) {
+        $title = valid_datas($_POST["title"]);
+        $description = valid_datas($_POST["description"]);
+        $codeName = valid_datas($_POST["codeName"]);
+        $country = valid_datas($_POST["country"]);
+        $startDate = ($_POST["startDate"]);
+        $endDate = ($_POST["endDate"]);
+        $specialityId = ($_POST["missionSpeciality"]);
+        $missionStatusId = ($_POST["missionStatus"]); 
+        $missionTypeId = ($_POST["missionType"]);
+        $agents = ($_POST["agents"]);
+        $contacts = ($_POST["contacts"]);
+        $targets = ($_POST["targets"]);
+        $safeHouses = ($_POST["safeHouses"]);     
+
+        // Récupère les objets de spécialité, statut de mission et type de mission à partir de leurs identifiants
+        $speciality = Speciality::getSpecialityById($pdo, $specialityId);
+        $missionStatus = MissionStatus::getMissionStatusById($pdo, $missionStatusId);
+        $missionType = MissionType::getMissionTypeById($pdo, $missionStatusId);
+
+        // Crée une nouvelle instance de la classe Mission
+        $mission = new Mission($pdo);
+
+        // Ajoute une nouvelle mission à la base de données
+        $newMission = $mission->addMission($title, $description, $codeName, $country, $startDate, $endDate, $speciality, $missionStatus, $missionType, $agents, $contacts, $targets, $safeHouses);
+
+        // Affiche un message de succès
+        echo "<div style='font-weight:bold;color:rgb(3, 114, 103)'>Nouvelle mission ajoutée en base de données</div>";
+
+        //Pause de 5 secondes
+        sleep(5);
+
+        // Redirection vers la page dashboardCreate.php
+        header('Location: ../views/dashboardCreate.php');
+        exit();
+
+    } else {
+        echo "Tous les champs sont requis.";
+    }
+} else {
+    echo "La requête doit être de type POST.";
+}
