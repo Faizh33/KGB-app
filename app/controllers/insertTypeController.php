1c5a21175e524f9a2168f7c84de692f5e51590e1
Erika REDON <erika_redon@hotmail.com>
2023-07-07 16:42:13 +0200

    creation of a controller to add a new type

---
+++ app/controllers/insertTypeController.php
@@ -0,0 +1,30 @@
+<?php
+
+use app\classes\MissionType;
+
+include_once "../../config/database.php";
+include_once "../helpers/dataHelpers.php";
+include_once "../classes/MissionType.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if(isset($_POST["type"])) {
+        //Vérifier si les informations sont passées dans le POST
+        $type = valid_datas($_POST["type"]);
+
+        //Création d'un nouvel objet MissionType et insertion des données
+        $typeObj = new MissionType($pdo);
+        $type = $typeObj::addMissionType($type);
+
+        //Si l'ajout en base de données à réussi : redirection vers la page de création
+        if(isset($type)) {
+            echo "<div style='font-weight:bold;color:rgb(3, 114, 103)'>Nouveau type de mission ajoutée en base de données</div>";
+            echo "<script>
+                setTimeout(function() {
+                    window.location.href = '../views/dashboardCreate.php';
+                }, 3000);
+            </script>";
+            exit;
+        }
+    }
+}
