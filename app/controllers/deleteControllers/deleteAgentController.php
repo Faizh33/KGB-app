819fc3c170c995294515f35e517392c38fb2917e
Erika REDON <erika_redon@hotmail.com>
2023-07-14 11:53:33 +0200

    remove  argument

--- app/controllers/deleteControllers/deleteAgentController.php
+++ app/controllers/deleteControllers/deleteAgentController.php
@@ -4,15 +4,15 @@ require '../../../config/database.php';
 
 use app\classes\Agent;
 
-if (isset($_POST['agentId']) && isset($_POST['specialities'])) {
+if (isset($_POST['agentId'])) {
     $agentId = $_POST['agentId'];
-    $specialities = $_POST['specialities'];
 
-    // Créez une instance de la classe Agent en utilisant le $pdo
+    // Créez une instance de la classe agent en utilisant le $pdo
     $agent = new Agent($pdo);
     // Chargez les détails de l'agent correspondant
     $agent->getAgentById($agentId);
-    // Supprimez l'agent
-    $agent->deleteAgentById($agentId, $specialityId);
+
+    // Supprimez l'agent lui-même
+    $agent->deleteAgentById($agentId);
 }
 ?>
