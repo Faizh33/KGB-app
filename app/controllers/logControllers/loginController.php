396911db0921d5055fb31292d22b4f362e3d7ca0
Erika REDON <erika_redon@hotmail.com>
2023-07-22 11:49:52 +0200

    protection against brute force attacks

--- app/controllers/logControllers/loginController.php
+++ app/controllers/logControllers/loginController.php
@@ -1,6 +1,24 @@
 <?php
 session_start();
 
+// Nombre maximum de tentatives infructueuses autorisées
+$maxLoginAttempts = 5;
+
+// Vérifier si l'utilisateur a dépassé le nombre maximum de tentatives infructueuses
+if (isset($_SESSION['login_attempts']) && $_SESSION['login_attempts'] >= $maxLoginAttempts) {
+    // Bloquer l'accès pour une période définie en ajoutant un délai à la prochaine tentative.
+    $blockDuration = 5 * 60;
+    $remainingTime = $blockDuration - (time() - $_SESSION['last_login_attempt']);
+    
+    if ($remainingTime > 0) {
+        echo "Vous avez dépassé le nombre maximum de tentatives de connexion. Veuillez réessayer dans " . $remainingTime . " secondes.";
+        exit();
+    } else {
+        // Réinitialisation du compteur de tentatives après la période de blocage
+        unset($_SESSION['login_attempts']);
+    }
+}
+
 require_once "../../../config/database.php";
 require_once "../../classes/Admin.php";
 use app\classes\Admin;
