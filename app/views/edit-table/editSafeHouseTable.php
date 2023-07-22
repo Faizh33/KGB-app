bb6dc2c765c5513c324774261704785cd61c3acf
Erika REDON <erika_redon@hotmail.com>
2023-07-22 11:33:14 +0200

    modification of the + svg to be responsive

--- app/views/edit-table/editSafeHouseTable.php
+++ app/views/edit-table/editSafeHouseTable.php
@@ -51,7 +51,7 @@ $safeHouses = $safeHouseObj::getAllSafeHousesPagination($page, $perPage);
     <?php if (isset($_SESSION['admin']) && $_SESSION['admin'] === true) : ?>
         <div class="plusItem">
             <a href="../dashboardCreate.php" class="plusLink">
-                <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="rgb(186, 238, 233)" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="3vw" height="3vw" fill="rgb(186, 238, 233)" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
                 </svg>
             </a>
