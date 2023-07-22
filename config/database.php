ec3338b878af28b124f3fac4d621b0ac66b7bdab
Erika REDON <erika_redon@hotmail.com>
2023-07-22 11:35:40 +0200

    database UTF8 encoding

--- config/database.php
+++ config/database.php
@@ -7,8 +7,10 @@ $username = "root";
 $password = "";
 
 try {
-    // Création de la connexion à la base de données
-    $pdo = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    // Création de la connexion à la base de données avec l'option UTF-8
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password, array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+    ));
 } catch (PDOException $e) {
     $errorMessage = "[" . date('Y-m-d H:i:s') . "] Erreur de connexion : " . $e->getMessage();
     error_log($errorMessage, 3, "../logs/error.log");
